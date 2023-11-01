6fb38b563e0e8e5f0d1994219fb90a8a3f9d21d2
Reiterovich <reiterovich.andriy@gmail.com>
2023-11-01 22:38:02 +0200

    ...

--- src/components/section/section.jsx
+++ src/components/section/section.jsx
@@ -2,7 +2,7 @@ import css from './section.module.css';
 
 export const Section = ({ title, children }) => {
   return (
-    <section>
+    <section className={css.section}>
       <h1>{title}</h1>
       {children}
     </section>
